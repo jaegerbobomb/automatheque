a24027d4d6494ce5b5a4b12a3142832d715a184f
marrco <marrco@wohecha.fr>
2023-10-30 16:29:02 +0100

    feat(courriel): Passe à attrs + tests

--- src/automatheque.schema/automatheque/schema/texte/courriel.py
+++ src/automatheque.schema/automatheque/schema/texte/courriel.py
@@ -1,25 +1,46 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
-from os.path import basename
-import os
+from email.header import Header
+from email.utils import formataddr, formatdate, parseaddr
+from typing import Callable, List, Sequence, Tuple, Union
 
 import attr
-from email.header import Header
-from email.utils import formatdate, formataddr, parseaddr
 
-#from automatheque.schema.medium import Medium
+# from automatheque.schema.medium import Medium
 
 
 @attr.s
-class Courriel():
+class Courriel:
     """Classe destinée à manipuler des courriels.
 
     Cette classe est un wrapper de "email".
-    """
-    EXTENSIONS = ['eml']
 
-    def __init__(self, appelant=None, sujet=None, contenu='',
-                 destinataires=None, emetteur=None, pieces_jointes=None):
+    La validité des emails est testée de manière très sommaire par défaut.
+    Si besoin, utiliser pyIsEmail ou autre librairie, à passer à "self.teste_adresse_valide".
+    Pour l'instant on se contente de ça : https://stackoverflow.com/a/14485817/8721626
+    """
+
+    _sujet: str = attr.ib(kw_only=True)
+    _emetteur: Union[tuple, str] = attr.ib(kw_only=True)
+    _destinataires: Sequence[Union[tuple, str]] = attr.ib(factory=list, kw_only=True)
+    contenu: str = attr.ib(init=False, kw_only=True)
+    pieces_jointes: List[str] = attr.ib(init=False, factory=list, kw_only=True)
+    _date_envoi: datetime = attr.ib(
+        init=False, default=None, kw_only=True
+    )  # TODO vérifier les timezones
+    teste_adresse_valide: Callable[[str], Tuple[bool, str]] = attr.ib(
+        init=False, default=lambda e: ("@" in e, ""), kw_only=True
+    )
+
+    def __init__(
+        self,
+        appelant=None,
+        sujet=None,
+        contenu="",
+        destinataires=None,
+        emetteur=None,
+        pieces_jointes=None,
+    ):
         """__init__.
 
         :param appelant:       le nom de l'appelant
@@ -29,40 +50,32 @@ class Courriel():
         :param emetteur:       adresse de courriel de l'emetteur
         :param pieces_jointes: liste des pièces jointes
         """
-        self.appelant = appelant
-        self.sujet = sujet if not appelant else '[{}] {}'.format(
-            appelant, sujet)
-        self.contenu = contenu
-        self.destinataires = destinataires
-        self._emetteur = emetteur
-        self.pieces_jointes = [] if not pieces_jointes else pieces_jointes
-        self.mimetext = 'html' if self.contenu.startswith('<html') else 'plain'
-        self._date_envoi = None  # Doit etre une datetime si pas vide # TODO vérifier les timezones
-        self._msg = None  # Message complet
-        
-    def _controle_format_email(self, valeur, champ):
+
+    def _controle_format_email(self, valeur: Union[tuple, str], champ: str):
         """Renvoie l'email contrôlé pour le champ indiqué.
-        
-        Conformément à https://docs.python.org/3/library/email.utils.html 
+
+        Conformément à https://docs.python.org/3/library/email.utils.html
         les emails doivent avoir un format particulier : soit une chaine que
         l'on peut décomposer avec parseaddr, soit un tuple que l'on peut
         formater avec formataddr.
-        
+
         :param valeur: tuple ou string contenant nom et email
         :param champ: champ à contrôler
         """
-        err = "{} doit etre une chaine ou un tuple conforme à parseaddr/formataddr cf: https://docs.python.org/3/library/email.utils.html".format(champ)
+        err = f"{champ} doit etre une chaine ou un tuple conforme à parseaddr/formataddr cf: https://docs.python.org/3/library/email.utils.html"  # noqa
         if isinstance(valeur, str):
-            (nom, email) = parseaddr(valeur)
-            if (nom, email) == ('', ''):
+            (nom, adresse) = parseaddr(valeur)
+            if (nom, adresse) == ("", ""):
                 raise ValueError(err)
         elif isinstance(valeur, tuple) and len(valeur) == 2:
-            (nom, email) = valeur
+            (nom, adresse) = valeur
         else:
             raise ValueError(err)
-        # TODO il faudrait aussi vérifier que email est conforme !!
-        return formataddr((str(Header(nom)), email))
-        
+        valide, raison = self.teste_adresse_valide(adresse)
+        if not valide:
+            raise ValueError(raison if raison else "Adresse courriel invalide")
+        return formataddr((str(Header(nom)), adresse))
+
     @property
     def emetteur(self):
         """Renvoie l'émetteur."""
@@ -72,7 +85,7 @@ class Courriel():
     def emetteur(self, valeur):
         """Vérifie que emetteur a un format correct avant affectation."""
         self._emetteur = self._controle_format_email(valeur, "emetteur")
-        
+
     @property
     def destinataires(self):
         """Renvoie les destinataires."""
@@ -92,7 +105,11 @@ class Courriel():
     def ajouter_destinataire(self, destinataire):
         """Ajoute un destinataire au message."""
         destinataire = self._controle_format_email(destinataire, "destinataire")
-        self.destinataires.append(destinataire)
+        self._destinataires.append(destinataire)
+
+    @property
+    def mimetext(self):
+        return "html" if self.contenu.startswith("<html") else "plain"
 
     @property
     def date_envoi(self):
@@ -104,7 +121,9 @@ class Courriel():
 
         TODO supprimer et mettre dans un StockageCourrielEsmtp par ex.
         """
-        res = u'Subject: {}\nMime-Version: 1.0;\nContent-Type: text/html; charset="ISO-8859-1";\nContent-Transfer-Encoding: 7bit;\n\n{}\n'.format(
-            self.sujet, self.contenu).encode('utf-8')
+        res = 'Subject: {}\nMime-Version: 1.0;\nContent-Type: text/html; charset="ISO-8859-1";\nContent-Transfer-Encoding: 7bit;\n\n{}\n'.format(
+            self.sujet, self.contenu
+        ).encode(
+            "utf-8"
+        )
         return res
-
--- src/automatheque.schema/tests/texte/test_courriel.py
+++ src/automatheque.schema/tests/texte/test_courriel.py
@@ -1,5 +1,23 @@
+import pytest  # type: ignore
+
 from automatheque.schema.texte.courriel import Courriel
 
+
 def test_courriel():
-    c = Courriel()
+    c = Courriel(emetteur="src@mail.com", sujet="Sujet du mail")
     assert isinstance(c, Courriel)
+
+    c.ajouter_destinataire("user@tld.com")
+    assert c.destinataires == ["user@tld.com"]
+    c.ajouter_destinataire("Toto <toto@tld.com>")
+    assert c.destinataires == ["user@tld.com", "Toto <toto@tld.com>"]
+    c.ajouter_destinataire(("Titi", "titi@tld.com"))
+    assert c.destinataires == [
+        "user@tld.com",
+        "Toto <toto@tld.com>",
+        "Titi <titi@tld.com>",
+    ]
+
+    with pytest.raises(ValueError) as err:
+        c.destinataires = ["Faux faux-mail.com"]
+    assert str(err.value) == "Adresse courriel invalide"
