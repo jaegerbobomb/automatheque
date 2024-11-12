065b14426fadbc42ddad1bc04146ebe515577355
marrco <marrco@wohecha.fr>
2024-11-12 21:29:59 +0100

    clean(automatheque.util)

--- src/automatheque/automatheque/util/fichier.py
+++ src/automatheque/automatheque/util/fichier.py
@@ -7,13 +7,13 @@ def enleve_caracteres_invalides(value):
 
     https://stackoverflow.com/questions/1033424/how-to-remove-bad-path-characters-in-python
     """
-    deletechars = '\/:*?"<>|'
+    deletechars = '/\:*?"<>|'
     # TODO tester la plateforme
     deletechars = "/"  # Linux autorise quasiment tout le reste
     try:
         for c in deletechars:
             value = value.replace(c, "_")
-    except Exception as e:
+    except Exception:
         # si la valeur est un int ou autre, on la renvoie telle quelle
         pass
     return value
